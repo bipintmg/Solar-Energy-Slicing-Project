a109cfeeb12590e995ccb2ddc13a85c429a041ac
Bipin <bipin@Bipins-MacBook-Pro.local>
2024-06-13 14:12:30 +0545

    first 3faq list expand

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,30 +2,69 @@
 
 var prevBtnID = new String();
 prevBtnID = null;
+var defOn = new Number();
+let defListID = new Number();
+let defListIDs = new Array(); // Holds the list ids to be expanded at default
+let defLiCount = 3; // Number of lists to expand on default
 
-function expand_collapse(liID) {
-  btnID = document
-    .getElementById(liID)
-    .firstElementChild.lastElementChild.getAttribute("id");
-  // Collapse logic
-  if (prevBtnID != null && prevBtnID != btnID) {
-    console.log(`collapsed: ${prevBtnID} \nexpanded: ${btnID}`);
-    expand(btnID);
-    collapse(prevBtnID);
-    prevBtnID = btnID;
-    // console.log(`After:-  collapsed: ${prevBtnID} and expanded: ${btnID}`);
-  } else if (prevBtnID != null && prevBtnID == btnID) {
-    collapse(btnID);
-    console.log(`collapsed: ${prevBtnID}`);
-    prevBtnID = null;
-    // console.log(`prevBtnID: ${prevBtnID} & btnID: ${btnID}`);
+document.addEventListener("DOMContentLoaded", () => {
+  let defLists = document.getElementsByClassName("faq__list")[0].children;
+  let i = 0;
+  for (let list of defLists) {
+    i += 1;
+    defListID = list.getAttribute("id");
+    console.log(defListID);
+    expand_collapse(defListID, true);
+    defListIDs.push(defListID);
+    if (i == defLiCount) break;
   }
-  // Expand logic
-  else {
-    expand(btnID);
-    console.log(`expanded: ${btnID}`);
-    prevBtnID = btnID;
-    // console.log(`prevBtnID: ${prevBtnID} & btnID: ${btnID}`);
+  console.log(defListIDs);
+  console.log("def");
+});
+
+function expand_collapse(liID, isDef) {
+  if (isDef) {
+    console.log(liID);
+    DefBtnID = document
+      .getElementById(liID)
+      .firstElementChild.lastElementChild.getAttribute("id");
+    expand(DefBtnID);
+    defOn = 1;
+  } else {
+    btnID = document
+      .getElementById(liID)
+      .firstElementChild.lastElementChild.getAttribute("id");
+
+    // Default first 3 collapse
+    if (defOn == 1) {
+      for (let listID of defListIDs) {
+        DefBtnID = document
+          .getElementById(listID)
+          .firstElementChild.lastElementChild.getAttribute("id");
+        collapse(DefBtnID, false);
+      }
+    }
+
+    // Collapse logic
+    if (prevBtnID != null && prevBtnID != btnID) {
+      console.log(`collapsed: ${prevBtnID} \nexpanded: ${btnID}`);
+      expand(btnID);
+      collapse(prevBtnID);
+      prevBtnID = btnID;
+      // console.log(`After:-  collapsed: ${prevBtnID} and expanded: ${btnID}`);
+    } else if (prevBtnID != null && prevBtnID == btnID) {
+      collapse(btnID);
+      console.log(`collapsed: ${prevBtnID}`);
+      prevBtnID = null;
+      // console.log(`prevBtnID: ${prevBtnID} & btnID: ${btnID}`);
+    }
+    // Expand logic
+    else {
+      expand(btnID);
+      console.log(`expanded: ${btnID}`);
+      prevBtnID = btnID;
+      // console.log(`prevBtnID: ${prevBtnID} & btnID: ${btnID}`);
+    }
   }
 }
 
