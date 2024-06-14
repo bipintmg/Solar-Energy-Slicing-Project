f40a1a6a4d0f24e9a44f78195ca4d3674fa25b13
Bipin <bipin@Bipins-MacBook-Pro.local>
2024-06-14 10:41:48 +0545

    faq restructured and fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,25 @@
-// Script for Single expandable accordion
+// // Script for Single expandable accordion
+document.addEventListener("DOMContentLoaded", function() {
+  var acc = document.getElementsByClassName("list__qn");
+  var panels = document.getElementsByClassName("list__ans");
 
-var prevBtnID = new String();
-prevBtnID = null;
-var defOn = new Number();
-let defListID = new Number();
-let defListIDs = new Array(); // Holds the list ids to be expanded at default
-let defLiCount = 3; // Number of lists to expand on default
+  // Activate the first three accordions
+  for (var i = 0; i < 3; i++) {
+      acc[i].classList.add("active");
+      panels[i].classList.add("show");
+  }
 
-document.addEventListener("DOMContentLoaded", () => {
-  let defLists = document.getElementsByClassName("faq__list")[0].children;
-  let i = 0;
-  for (let list of defLists) {
-    i += 1;
-    defListID = list.getAttribute("id");
-    expand_collapse(defListID, true);
-    defListIDs.push(defListID);
-    if (i == defLiCount) break;
+  // Add click event listener to all accordions
+  for (var i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          panel.classList.toggle("show");
+      });
   }
 });
 
-function expand_collapse(liID, isDef) {
-  if (isDef) {
-    DefBtnID = document
-      .getElementById(liID)
-      .firstElementChild.lastElementChild.getAttribute("id");
-    expand(DefBtnID);
-    defOn = 1;
-  } else {
-    btnID = document
-      .getElementById(liID)
-      .firstElementChild.lastElementChild.getAttribute("id");
-
-    // Default first 3 collapse
-    if (defOn == 1) {
-      for (let listID of defListIDs) {
-        DefBtnID = document
-          .getElementById(listID)
-          .firstElementChild.lastElementChild.getAttribute("id");
-        collapse(DefBtnID, false);
-      }
-    }
-
-    // Collapse logic
-    if (prevBtnID != null && prevBtnID != btnID) {
-      expand(btnID);
-      collapse(prevBtnID);
-      prevBtnID = btnID;
-    } else if (prevBtnID != null && prevBtnID == btnID) {
-      collapse(btnID);
-      prevBtnID = null;
-    }
-    // Expand logic
-    else {
-      expand(btnID);
-      prevBtnID = btnID;
-    }
-  }
-}
-
-function expand(btnID) {
-  let btnClicked = document.getElementById(btnID);
-  let pathVertical = btnClicked.children[0].firstElementChild;
-  let content = btnClicked.parentElement.parentElement.lastElementChild;
-
-  pathVertical.style.display = "none";
-  content.style.display = "block";
-}
-
-function collapse(btnID) {
-  let prevBtn = document.getElementById(btnID);
-  let pathVertical = prevBtn.children[0].firstElementChild;
-  let content = prevBtn.parentElement.parentElement.lastElementChild;
-
-  pathVertical.style.display = "block";
-  content.style.display = "none";
-}
-
+//menu
 let burger_toggle = 0;
 
 function burger() {
